de51f450b56d426984ee950116abc75dec612ad4
Fahim-BAUST <ashhabf50@gmail.com>
2021-11-15 22:53:08 +0600

    add

--- index.js
+++ index.js
@@ -47,12 +47,12 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
-        // app.get('/products/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await productCollection.findOne(query);
-        //     res.send(result);
-        // })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        })
 
         app.get('/user/:email', async (req, res) => {
             const email = req.params.email;
